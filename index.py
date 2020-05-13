6cd1934c2a65714d9b177eca63af5ad0bbbafacb
hyunkyung <tbgusrud12@gmail.com>
2020-05-14 02:59:37 +0900

    delete

--- index.py
+++ index.py
@@ -13,6 +13,12 @@ if 'id' in form:
     pageId = form["id"].value
     description = open('data/'+pageId, 'r').read()
     update_link = '<a href="update.py?id={}">update</a>'.format(pageId)
+    delete_action = '''
+        <form action="process_delete.py" method="post">
+            <input type="hidden" name="pageId" value="{}">
+            <input type="submit" value="delete">
+        </form>
+    '''.format(pageId)
 else:
     pageId = 'Welcome'
     description = 'Hello, web'
@@ -30,9 +36,10 @@ print('''<!doctype html>
   </ol>
   <a href="create.py">create</a>
   {update_link}
+  {delete_action}
   <a href="update.py">update</a>
   <h2>{title}</h2>
   <p>{desc}</p>
 </body>
 </html>
-'''.format(title=pageId, desc=description, listStr=listStr,update_link=update_link))
+'''.format(title=pageId, desc=description, listStr=listStr,update_link=update_link, delete_action=delete_action))
